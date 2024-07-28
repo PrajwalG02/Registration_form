d29aa61dd38f1726f55234efbd424b61846c23bc
PrajwalG02 <139797158+PrajwalG02@users.noreply.github.com>
2024-07-28 15:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
-
-// Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/DataBase', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
-
-// User schema
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -22,27 +18,23 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public')); // Serve static files
+app.use(express.static('public')); 
 
-// Register route
 app.post('/register', (req, res) => {
   const { name, email, password, phone, age, gender } = req.body;
 
   const newUser = new User({ name, email, password, phone, age, gender });
   newUser.save()
     .then(() => {
-      res.redirect('success.html'); // Redirect to success page
+      res.redirect('success.html'); 
     })
     .catch(err => {
       console.error(err);
-      // Handle error, e.g., send an error message to the client
       res.status(500).send('Error registering user');
     });
 });
 
-// Serve the success page
 app.get('success.html', (req, res) => {
   res.sendFile(__dirname + 'success.html');
 });
